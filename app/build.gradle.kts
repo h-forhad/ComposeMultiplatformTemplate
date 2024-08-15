8b193d223d49cbba51947fa6ea3aa31da58bbb6b
WHKQ63 <mohammadforhad.hossain@motorolasolutions.com>
2024-08-15 16:02:51 +0800

    Add android project module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.androidApplication)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.jetbrainsCompose)
 }
@@ -19,18 +19,47 @@ kotlin {
 
     sourceSets.commonMain.dependencies {
         api(project(":decompose-router"))
+        implementation(compose.ui)
         implementation(compose.foundation)
         implementation(compose.material3)
         implementation(compose.runtime)
+        implementation(compose.materialIconsExtended)
+
+
+        implementation(libs.kotlin.corotines)
+        implementation(libs.kotlin.datetime)
+        implementation(libs.ktor.core)
+        implementation(libs.ktor.serialization)
+        implementation(libs.ktor.logging)
+
+        implementation(libs.koin.core)
+        implementation(libs.molecule.runtime)
+        implementation(libs.kstore)
+
+        implementation(libs.qdsfdhvh.image.loader)
+        implementation(libs.google.services.map)
+        implementation(libs.decompose.compose)
+        implementation(libs.calendar.compose.basis)
+        implementation(libs.calendar.compose.ranges) // includes basis
+        implementation(libs.calendar.compose.pager) // includes basis
+        implementation(libs.calendar.compose.datepicker) // includes pager + ranges
     }
 
     sourceSets.androidMain.dependencies {
-        implementation("androidx.activity:activity-compose:1.9.0")
-        implementation("androidx.appcompat:appcompat:1.7.0")
+        implementation(libs.androidx.activity.compose)
+        implementation(libs.ktor.client)
+        implementation(libs.ktor.client.okhttp)
+        implementation(libs.kstore.file)
+        implementation(libs.androidx.appcompat)
     }
 
     sourceSets.jvmMain.dependencies {
         implementation(compose.desktop.currentOs)
+    }
+
+    sourceSets.iosMain.dependencies {
+        implementation(libs.kstore.file)
+        implementation(libs.ktor.client.ios)
     }
 }
 
