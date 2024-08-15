ec1f2509bc9af821e610a584508881d390053568
WHKQ63 <mohammadforhad.hossain@motorolasolutions.com>
2024-08-15 16:06:34 +0800

    Add ios project module

--- app/iosApp/kmpSample/kmpSample/kmpSampleApp.swift
+++ app/iosApp/kmpSample/kmpSample/kmpSampleApp.swift
@@ -1,28 +1,47 @@
-//
-//  kmpSampleApp.swift
-//  kmpSample
-//
-//  Created by Sebastian Sellmair on 17.07.24.
-//
-
 import SwiftUI
-import KmpApp
-
-struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> some UIViewController {
-        SampleAppViewControllerKt.createViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
-    }
-}
+import WanderWise
 
 @main
 struct kmpSampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ComposeView()
+  @UIApplicationDelegateAdaptor var delegate: AppDelegate
+  @Environment(\.scenePhase)  var scenePhase: ScenePhase
+
+  var defaultRouterContext: RouterContext { delegate.holder.defaultRouterContext }
+
+  var body: some Scene {
+    WindowGroup {
+      ComposeView(routerContext: defaultRouterContext)
+    }
+    .onChange(of: scenePhase) { newPhase in
+        switch newPhase {
+        case .background: defaultRouterContext.stop()
+        case .inactive: defaultRouterContext.pause()
+        case .active: defaultRouterContext.resume()
+        @unknown default: break
         }
     }
+  }
+}
+
+class DefaultRouterHolder : ObservableObject {
+  let defaultRouterContext: RouterContext = DefaultRouterContextKt.defaultRouterContext()
+
+  deinit {
+    // Destroy the root component before it is deallocated
+    defaultRouterContext.destroy()
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let holder: DefaultRouterHolder = DefaultRouterHolder()
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+  let routerContext: RouterContext
+
+  func makeUIViewController(context: Context) -> UIViewController {
+    return SampleAppViewControllerKt.createViewController(routerContext: routerContext)
+  }
+
+  func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
--- app/src/iosMain/kotlin/com/greenrobotdev/wanderwise/SampleAppViewController.kt
+++ app/src/iosMain/kotlin/com/greenrobotdev/wanderwise/SampleAppViewController.kt
@@ -1,8 +1,35 @@
 package com.greenrobotdev.wanderwise
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
+import com.greenrobotdev.wanderwise.screen.home.HomeScreen
+import io.github.xxfast.decompose.router.LocalRouterContext
+import io.github.xxfast.decompose.router.RouterContext
 
 @Suppress("unused") // used by swift code
-fun createViewController() = ComposeUIViewController {
-    MainScreen()
+fun createViewController(
+    routerContext: RouterContext
+) = ComposeUIViewController {
+    BoxWithConstraints(
+        modifier = Modifier
+            .windowInsetsPadding(WindowInsets.statusBars)
+            .windowInsetsPadding(WindowInsets.navigationBars)
+    ) {
+
+        CompositionLocalProvider(
+            LocalRouterContext provides routerContext,
+        ) {
+            Surface(tonalElevation = 5.dp) {
+                HomeScreen()
+            }
+        }
+    }
 }
